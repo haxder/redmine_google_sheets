221cbe6faa7531dbe77993291c3e99f761f8eab2
Nguyen Duc Tuan <nd-tuan@ios-net.co.jp>
2019-12-10 13:43:10 +0700

    update

--- src/main/java/Redmine/Redmine.java
+++ src/main/java/Redmine/Redmine.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class Redmine {
     private static String API_KEY = "d821ff33a0c1f69b9b901564948f71cefe276f6c";
     private static String REDMINE_URL = "https://www.ios-developer001.com/redmine";
-    private static List<String> IGNORE_LIST= Arrays.asList("【ＱＡ内容】","【QA内容】");
+    private static List<String> IGNORE_LIST = Arrays.asList("【ＱＡ内容】", "【QA内容】");
     private static IssueManager issueManager = createIssueManager();
 
     private static IssueManager createIssueManager() {
@@ -39,34 +39,20 @@ public class Redmine {
         Params params = new Params()
                 .add("assigned_to_id", "59")
                 .add("limit", "1000");
+
         try {
             results = issueManager.getIssues(params).getResults();
             Iterator itr = results.iterator();
-            while (itr.hasNext()){
+            while (itr.hasNext()) {
                 Issue temp = (Issue) itr.next();
-                for (String ig: IGNORE_LIST) {
-                    if(temp.getDescription().contains(ig)){
-                        itr.remove();
-                    }
+                if (temp.getDescription().contains("【ＱＡ内容】") || temp.getDescription().contains("【QA内容】")) {
+                    itr.remove();
                 }
             }
-            return  results;
+            return results;
         } catch (RedmineException e) {
             e.printStackTrace();
         }
         return null;
     }
-/*
-    public static void main(String[] args) {
-        List<Issue> issues = getIssuesAssgineME();
-        Iterator itr = issues.iterator();
-        while (itr.hasNext()){
-            Issue temp = (Issue) itr.next();
-            if(temp.getDueDate() != null) {
-                String dateStr = DateUtils.date2LocalDate(temp.getDueDate(), ZoneId.of("+9")).toString();
-                System.out.println(dateStr);
-            }
-            System.out.println("end");
-        }
-    }*/
 }
